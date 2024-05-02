7e9bd8b219be3a5ec9f5ee0ae56b3dafd3cff7c6
Daffa Setya Ramadhan <113727851+Akitasenn@users.noreply.github.com>
2024-05-02 23:24:27 +0700

    Add files via upload

---
+++ Inheritance_Daffa Setya Ramadhan_A11.2022.14042/Person.java
@@ -0,0 +1,148 @@
+package inheritance;
+
+public class Person {
+    protected String name;
+    protected String address;
+
+    //Default Constructor
+    public Person() {
+        // System.out.println("Inside Person:Constructor");  // Commented out to prevent output
+        name = "";
+        address = "";
+    }
+
+    //Constructor with parameters
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void job(String job) {
+        System.out.println("Pekerjaan " + job);
+    }
+
+    public void identity() {
+        System.out.println("Nama: " + name);
+        System.out.println("Alamat: " + address);
+    }
+
+    public void hobi(String hobi) {
+        System.out.println("Hobi: " + hobi);
+    }
+}
+
+
+// package inheritance;
+
+// public class Person {
+//     protected String name;
+//     protected String address;
+
+//     //Default Constructor
+//     public Person() {
+//         System.out.println("Inside Person:Constructor");
+//         name = "";
+//         address = "";
+//     }
+
+//     //Constructor dengan parameter
+//     public Person(String name, String address) {
+//         this.name = name;
+//         this.address = address;
+//     }
+
+//     public String getName() {
+//         return name;
+//     }
+
+//     public String getAddress() {
+//         return address;
+//     }
+
+//     public void setName(String name) {
+//         this.name = name;
+//     }
+
+//     public void setAddress(String address) {
+//         this.address = address;
+//     }
+
+//     public void job(String job) {
+//         System.out.println("Pekerjaan " + job);
+//     }
+
+//     public void identity() {
+//         System.out.println("Nama: " + name);
+//         System.out.println("Alamat: " + address);
+//     }
+
+//     // New method to display hobbies
+//     public void hobi(String hobi) {
+//         System.out.println("Hobi: " + hobi);
+//     }
+// }
+
+
+// default code
+// package inheritance;
+
+// public class Person {
+// 	protected String name;
+// 	protected String address;
+	
+// 	//Default Constructor
+// 	public Person() {
+// 		System.out.println("Inside Person:Constructor");
+// 		name="";
+// 		address="";
+// 	}
+	
+// 	//Constructor dengan parameter
+// 	public Person(String name,String address) {
+// 		this.name = name;
+// 		this.address = address;
+// 	}
+	
+// 	public String getName() {
+// 		return name;
+// 	}
+	
+// 	public String getAddress() {
+// 		return address;
+// 	}
+	
+// 	public void setName(String name) {
+// 		this.name = name;
+// 	}
+	
+// 	public void setAddress(String address) {
+// 		this.address = address;
+// 	}
+	
+// 	public void job(String job)
+// 	{
+// 		System.out.println("Pekerjaan "+job);
+// 	}
+	
+// 	public void identity()
+// 	{
+// 		System.out.println("Nama: "+name);
+// 		System.out.println("Alamat: "+address);
+// 	}
+// }
